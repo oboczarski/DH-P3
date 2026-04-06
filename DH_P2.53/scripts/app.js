26053fdc06b4a54f39c4977f750fc2df085a1664
oscarbrandsllc <oscarbrands.llc@gmail.com>
2026-04-06 08:36:32 -0700

    Refactor code structure for improved readability and maintainability

--- DH_P2.53/scripts/app.js
+++ DH_P2.53/scripts/app.js
@@ -374,6 +374,10 @@ const getPageUrl = (page) => {
         case 'research':
             url = `${base}research/research.html`;
             break;
+        case 'datahub':
+            // DataHub standalone stats/analytics page
+            url = `${base}datahub/DataHub.html`;
+            break;
         case 'contact':
             url = `${base}contact/contact.html`;
             break;
